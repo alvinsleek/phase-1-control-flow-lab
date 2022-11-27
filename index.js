13f19dcbc993ed31b34e1631119836121ac7ce81
Alvin Mithamo <alvinmithamo100@gmail.com>
2022-11-27 14:45:08 +0300

    control flow

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  let distance
+    if(somevalue <= 400){
+        distance = "This is on me!";
+    }else {
+        distance = "You gotta pay for it"
+    }
+    return result
+
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+  const distance = 400;
+    
+  const distanceMessage = distance > 400 ? "Your destination is far!" : "Your destination is close!";
+
+  distanceMessage;
+
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  const tip = 300;
+
+  let amountMessage;
+
+  switch (tip) {
+      case 100:
+      case 200:
+      case 300:
+      case 400:
+      case 500:
+      case 600:
+          amountMessage = "Thank you so much!";
+          break;
+      default:
+          amountMessage = "I appreciate";    
+         
 }
